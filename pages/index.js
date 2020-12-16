a0b07a81901690d93227633ec8a3e36ab9069767
Manish Mandal <manish.mandal21@gmail.com>
2020-12-17 00:48:10 +0530

    fixed card error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import Card from '../components/card/card'
+import Card from '../components/card/Card'
 import Button from '../components/Button'
 
 export default function Home(props) {
